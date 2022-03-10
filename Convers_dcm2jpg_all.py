bedd8caf7251a08c394394e14d0479f46a48503e
Liolik <allesid@gmail.com>
2022-03-10 23:17:40 +0300

    Add 3D to file

--- Convers_dcm2jpg_all.py
+++ Convers_dcm2jpg_all.py
@@ -65,7 +65,6 @@ def convert2D_dcm2jpg(dcm_folder_path="DCM_imgs_2D", jpg_folder_path="JPG_imgs_2
         print(f"File {dicom_image_description_file} is absent.")
         sys.exit(0)
 
-
     # Conversion
     images_path = os.listdir(dcm_folder_path)
     for image in images_path:
@@ -77,55 +76,60 @@ def convert2D_dcm2jpg(dcm_folder_path="DCM_imgs_2D", jpg_folder_path="JPG_imgs_2
             img = ds.pixel_array
             # print('pixel_array_numpy.shape=', pixel_array_numpy.shape)
                 # print('img=\n', img)
-            if len(img.shape) != 2:
-                print(f"Input error: {image} file is {len(img.shape)} dimentional, \
-                expected 2 dimentions,\nfile is missing")
-                continue
+            if len(img.shape) == 2:
             # print("name: ", image)
             # img -= img[0, 0]
-            lp = len(params)
-            # print('lp=', lp)
-            # extract image information
-            for field in pc:
-                # print('field0=', field)
-                try:
-                    if ds.data_element(field) is None:
+                lp = len(params)
+                # print('lp=', lp)
+                # extract image information
+                for field in pc:
+                    # print('field0=', field)
+                    try:
+                        if ds.data_element(field) is None:
+                            params.loc[lp, field] = None
+                        else:
+                            s1 = str(ds.data_element(field)).replace("'", "")
+                            s2 = s1.find(":")
+                            s1 = s1[s2+2:]
+                            params.loc[lp, field] = s1
+                    except:
                         params.loc[lp, field] = None
-                    else:
-                        s1 = str(ds.data_element(field)).replace("'", "")
-                        s2 = s1.find(":")
-                        s1 = s1[s2+2:]
-                        params.loc[lp, field] = s1
-                except:
-                    params.loc[lp, field] = None
-            if annots:
-                if params.loc[lp, "SOPInstanceUID"] in list(annots["instanceUID"]):
-                    sopii = params.loc[lp, "SOPInstanceUID"]
-                    print('sopii=', sopii, ' type sopii=', type(sopii))
-                    ann = annots[annots["instanceUID"]
-                                == sopii].reset_index()
-                    print(ann)
-                    for i in range(len(ann)):
-                        xstart = ann.loc[i, "start_x"]
-                        ystart = ann.loc[i, "start_y"]
-                        xend = ann.loc[i, "end_x"]
-                        yend = ann.loc[i, "end_y"]
-                        print("xy=", xstart, ystart, xend, yend)
-                        img = rectangle(img, (xstart, ystart),
-                                        (xend, yend), img.max())
-            imgmax = 2000
-            vfun = np.vectorize(lambda x: x if x <= imgmax else imgmax)
-            img = vfun(img)
-            img = (img / img.max()) * 255
-            # img = (img - img.min()) / (img.max() - img.min()) * 255
-            image = image.replace('.dcm', '')
-            cv2.imwrite(os.path.join(jpg_folder_path,
-                        image+'.jpg'), img)
-        print(f"{ds.filename} converted to jpg file, img.min= {img.min()}, img.max= {img.max()}")
-        params = params.dropna(axis=1, how='all')
-        # information about images saved in file Images2D_information.csv
-        params.to_csv(os.path.join(jpg_folder_path, ds.PatientID + "-" + img_info_fn))
-    
+                if annots:
+                    if params.loc[lp, "SOPInstanceUID"] in list(annots["instanceUID"]):
+                        sopii = params.loc[lp, "SOPInstanceUID"]
+                        print('sopii=', sopii, ' type sopii=', type(sopii))
+                        ann = annots[annots["instanceUID"]
+                                    == sopii].reset_index()
+                        print(ann)
+                        for i in range(len(ann)):
+                            xstart = ann.loc[i, "start_x"]
+                            ystart = ann.loc[i, "start_y"]
+                            xend = ann.loc[i, "end_x"]
+                            yend = ann.loc[i, "end_y"]
+                            print("xy=", xstart, ystart, xend, yend)
+                            img = rectangle(img, (xstart, ystart),
+                                            (xend, yend), img.max())
+                imgmax = 2000
+                vfun = np.vectorize(lambda x: x if x <= imgmax else imgmax)
+                img = vfun(img)
+                img = (img / img.max()) * 255
+                # img = (img - img.min()) / (img.max() - img.min()) * 255
+                image = image.replace('.dcm', '')
+                cv2.imwrite(os.path.join(jpg_folder_path,
+                            image+'.jpg'), img)
+            elif len(img.shape) == 3:
+                pass
+            else:
+                print(f"Input error: {image} file is {len(img.shape)} dimentional, \
+                expected 2 or 3 dimentions,\nfile is missing")
+                continue
+            print(
+                f"{ds.filename} converted to jpg file, img.min= {img.min()}, img.max= {img.max()}")
+            params = params.dropna(axis=1, how='all')
+            # information about images saved in file Images2D_information.csv
+            params.to_csv(os.path.join(jpg_folder_path,
+                          ds.PatientID + "-" + img_info_fn))
+
 
 def take_dcm_files(dcm_folder_path="DCM_imgs", jpg_folder_path="JPG_imgs", conv_fun=convert2D_dcm2jpg):
     """
